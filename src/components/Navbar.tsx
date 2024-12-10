36e7f29face19bb94c5f2fc5c918d6be565401ce
karananeja <karananeja.r@gmail.com>
2024-12-10 23:17:05 +0530

    fix: styling changes in the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,23 +15,23 @@ const Navbar = () => {
 
       <ul className='hidden md:flex items-start gap-5 font-medium'>
         <NavLink to='/'>
-          <li className='py-1 '>HOME</li>
-          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
+          <li className='py-1'>HOME</li>
+          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto invisible' />
         </NavLink>
 
         <NavLink to='/doctors'>
-          <li className='py-1 '>ALL DOCTORS</li>
-          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
+          <li className='py-1'>ALL DOCTORS</li>
+          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto invisible' />
         </NavLink>
 
         <NavLink to='/about'>
-          <li className='py-1 '>ABOUT</li>
-          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
+          <li className='py-1'>ABOUT</li>
+          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto invisible' />
         </NavLink>
 
         <NavLink to='/contact'>
-          <li className='py-1 '>CONTACT</li>
-          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
+          <li className='py-1'>CONTACT</li>
+          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto invisible' />
         </NavLink>
       </ul>
 
