7ff9aa8ebadf554abd4bc45831bf67f1b3f7695e
karananeja <karananeja.r@gmail.com>
2024-12-10 17:19:21 +0530

    chore: create the base route and add the required assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
+import { Route, Routes } from 'react-router-dom';
+
+import About from './pages/About';
+import Appointment from './pages/Appointment';
+import Contact from './pages/Contact';
+import Doctors from './pages/Doctors';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import MyAppointments from './pages/MyAppointments';
+import MyProfile from './pages/MyProfile';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className='mx-4 sm:mx-[10%]'>
+      App
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/doctors/:specialty' element={<Doctors />} />
+        <Route path='/my-profile' element={<MyProfile />} />
+        <Route path='/my-appointments' element={<MyAppointments />} />
+        <Route path='/appointment/:docId' element={<Appointment />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
