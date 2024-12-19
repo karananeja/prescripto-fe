2ccdccc5075b302745c40ef40ea4f099d4c244ae
karananeja <karananeja.r@gmail.com>
2024-12-20 00:04:03 +0530

    fix: update the specialty of few doctors

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -164,7 +164,7 @@ export const doctors = [
     _id: 'doc6',
     name: 'Dr. Andrew Williams',
     image: doc6,
-    specialty: 'Neurologist',
+    specialty: 'Gastroenterologist',
     degree: 'MBBS',
     experience: '4 Years',
     about:
@@ -254,7 +254,7 @@ export const doctors = [
     _id: 'doc12',
     name: 'Dr. Patrick Harris',
     image: doc12,
-    specialty: 'Neurologist',
+    specialty: 'Gastroenterologist',
     degree: 'MBBS',
     experience: '4 Years',
     about:
