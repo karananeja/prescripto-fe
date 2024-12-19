c5f448bfdc096b2b3858ad604d55922e88d54d60
karananeja <karananeja.r@gmail.com>
2024-12-19 23:19:37 +0530

    fix: add the navigate on the app logo in the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,12 @@ export const Navbar = () => {
 
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
-      <img className='w-44 cursor-pointer' src={assets.logo} alt='app-logo' />
+      <img
+        onClick={() => navigate('/')}
+        className='w-44 cursor-pointer'
+        src={assets.logo}
+        alt='app-logo'
+      />
 
       <ul className='hidden md:flex items-start gap-5 font-medium'>
         <NavLink to='/'>
