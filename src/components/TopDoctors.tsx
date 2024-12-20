b7e7d43729111c3a950ce3287f2daa854c571808
karananeja <karananeja.r@gmail.com>
2024-12-20 10:49:45 +0530

    feat: add scrollTo function in the appointment page

--- src/components/TopDoctors.tsx
+++ src/components/TopDoctors.tsx
@@ -16,7 +16,10 @@ export const TopDoctors = () => {
       <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
         {doctors.slice(0, 10).map((doctor) => (
           <div
-            onClick={() => navigate(`/appointment/${doctor._id}`)}
+            onClick={() => {
+              navigate(`/appointment/${doctor._id}`);
+              scrollTo(0, 0);
+            }}
             className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500'
             key={doctor._id}
           >
