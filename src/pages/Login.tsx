0fe6be7a6b48af37f8b357b101ae0a3d81321f81
karananeja <karananeja.r@gmail.com>
2024-12-21 14:29:37 +0530

    chore: complete the login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,85 @@
+import { FormEvent, useState } from 'react';
+
 export const Login = () => {
-  return <div>Login</div>;
+  const [isLogin, setIsLogin] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log({ email, password, name });
+  };
+
+  return (
+    <form className='min-h-[80vh] flex items-center' onSubmit={handleSubmit}>
+      <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-80 sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg'>
+        <p className='text-2xl font-semibold'>
+          {isLogin ? 'Login' : 'Create Account'}
+        </p>
+        <p>Please {isLogin ? 'log in' : 'sign up'} to book appointment</p>
+
+        {!isLogin ? (
+          <div className='w-full'>
+            <p>Full Name</p>
+            <input
+              className='border border-zinc-300 rounded w-full p-2 mt-1'
+              type='text'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </div>
+        ) : null}
+
+        <div className='w-full'>
+          <p>Email</p>
+          <input
+            className='border border-zinc-300 rounded w-full p-2 mt-1'
+            type='email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+
+        <div className='w-full'>
+          <p>Password</p>
+          <input
+            className='border border-zinc-300 rounded w-full p-2 mt-1'
+            type='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+
+        <button className='bg-primary text-white w-full py-2 my-2 rounded-md text-base'>
+          {isLogin ? 'Login' : 'Create Account'}
+        </button>
+
+        {isLogin ? (
+          <p>
+            Create a new account?{' '}
+            <span
+              className='text-primary underline cursor-pointer'
+              onClick={() => setIsLogin(!isLogin)}
+            >
+              Click here
+            </span>
+          </p>
+        ) : (
+          <p>
+            Already have an account?{' '}
+            <span
+              className='text-primary underline cursor-pointer'
+              onClick={() => setIsLogin(!isLogin)}
+            >
+              Login here
+            </span>
+          </p>
+        )}
+      </div>
+    </form>
+  );
 };
