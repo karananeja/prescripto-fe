d576b77fbccde0a7a91d534fa8f5c81b5250d234
karananeja <karananeja.r@gmail.com>
2024-12-21 13:23:14 +0530

    chore: complete the contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,3 +1,38 @@
+import { assets } from '../assets/assets';
+
 export const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <>
+      <p className='text-center text-2xl pt-10 text-gray-500'>
+        CONTACT <span className='text-gray-700 font-semibold'>US</span>
+      </p>
+
+      <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28 text-sm'>
+        <img
+          className='w-full md:max-w-[22.5rem]'
+          src={assets.contact_image}
+          alt='contact-logo'
+        />
+
+        <div className='flex flex-col justify-center items-start gap-6'>
+          <p className='font-semibold text-lg text-gray-600'>OUR OFFICE</p>
+          <p className='text-gray-500'>
+            00000 Williams Station <br /> Suite 000, Washington, USA
+          </p>
+          <p className='text-gray-500'>
+            Tel: (000) 000-0000 <br /> Email: johnsmith@email.com
+          </p>
+          <p className='font-semibold text-lg text-gray-600'>
+            CAREERS AT PRESCRIPTO
+          </p>
+          <p className='text-gray-500'>
+            Learn more about our teams and job openings.
+          </p>
+          <button className='border border-primary px-8 py-4 text-sm rounded-full hover:bg-primary hover:text-white transition-all duration-300'>
+            Explore Jobs
+          </button>
+        </div>
+      </div>
+    </>
+  );
 };
